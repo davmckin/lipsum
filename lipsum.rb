bb9bc15c7e04968c7603b1ce51584f2618a4a3c8
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-01-30 15:41:01 -0500

    adding a better empty option

--- lipsum.rb
+++ lipsum.rb
@@ -1,9 +1,13 @@
 lipsum_wanted = ARGV[0]
 
-if ARGV[1]
+if ARGV[0].empty?
+  puts "Hey man...maybe type an arguement (something, ANYTHING) and see what happens."
+end
+
+if ARGV[2]
   puts "How much did you make me? Um, is this the boring, peaceful kind of taking to the streets? You are the last hope of the universe. Soothe us with sweet lies"
-elsif ARGV[0]
+elsif ARGV[1]
   puts "Say it in Russian! I found what I need. And it's not friends, it's things. I'm Santa Claus! Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you. A true inspiration for the children"
 else
-  puts "Hey man...maybe type an arguement (something, ANYTHING) and see what happens."
+  puts "Too many words in your arguement. I'm out."
 end
