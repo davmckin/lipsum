3419d6e1c66ae5a801e2a7e70111e887635c605d
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-01-30 15:08:19 -0500

    commit message

--- lipsum.rb
+++ lipsum.rb
@@ -1,1 +1,9 @@
-2*3
+lipsum_wanted = ARGV[0]
+
+if ARGV[1]
+  puts "How much did you make me? Um, is this the boring, peaceful kind of taking to the streets? You are the last hope of the universe. Soothe us with sweet lies"
+elsif ARGV[0]
+  puts "Say it in Russian! I found what I need. And it's not friends, it's things. I'm Santa Claus! Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you. A true inspiration for the children"
+else
+  puts "Hey man...maybe type an arguement (something, ANYTHING) and see what happens."
+end
